d105ab1821f2f1c402d39e5707f7462b9ff20269
anurag <kr.anurag24@gmail.com>
2021-12-29 14:47:58 +0530

    changes

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const Discord = require('discord.js')
-const { MessageEmbed } = require('discord.js');
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
-require("dotenv").config()
+require("dotenv").config();
+
+const PREFIX = "?";
 
 const client = new Discord.Client({
   allowedMentions: {
-    parse: ['users', 'roles'],
+    parse: ["users", "roles"],
     repliedUser: true,
-
   },
   intents: [
     "GUILDS",
@@ -15,17 +16,41 @@ const client = new Discord.Client({
     "GUILD_PRESENCES",
     "GUILD_MEMBERS",
     "GUILD_MESSAGE_REACTIONS",
-  ]
-})
+  ],
+});
 
-client.on('ready', () => {
-  console.log("Tada, I'm ready")
-})
+client.on("ready", () => {
+  console.log("Tada, I'm ready");
+});
 
-client.on('message', async message => {
-  if (message.content === '$ping') {
-    message.reply('pong!')
+client.on("message", async (message) => {
+  if (message.content === `${PREFIX}ping`) {
+    message.reply("pong!");
   }
-})
+});
 
-client.login(process.env[`TOKEN`])
+
+// client.on("message", async (message) => {
+//   if (message.content === `${PREFIX}kick`) {
+//     const member = message.mentions.members.first();
+//     if (!member) {
+//       return message.reply("Please mention a valid member of this server");
+//     }
+//     if (!member.kickable) {
+//       return message.reply(
+//         "I cannot kick this user! Do they have a higher role? Do I have kick permissions?"
+//       );
+//     }
+//     await member.kick();
+
+//     const embed = new MessageEmbed()
+//       .setTitle("Kicked")
+//       .setDescription(`${member.user.tag} has been kicked`)
+//       .setColor("#ff0000")
+//       .setTimestamp();
+
+//     message.channel.send(embed);
+//   }
+// });
+
+client.login(process.env[`TOKEN`]);
